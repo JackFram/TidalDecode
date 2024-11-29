b0403b45a6e8d9486fadfd337344e69231d9e7ff
Zhihao Zhang <zhihaoz3@catalyst-0-15.eth>
2024-11-29 11:46:10 -0500

    kernel impl supported for llama3.x

--- tidal/models/TDAttention.py
+++ tidal/models/TDAttention.py
@@ -16,10 +16,10 @@ from transformers.models.llama.modeling_llama import (
 import tidal.utils
 
 
-class SearchAttention(nn.Module):
+class TDAttention(nn.Module):
     """Multi-headed attention from 'Attention Is All You Need' paper"""
 
-    def __init__(self, config: LlamaConfig, layer_idx: int):
+    def __init__(self, config: LlamaConfig, layer_idx: int, att_type="full"):
         super().__init__()
         self.layer_idx = layer_idx
         self.config = config
@@ -30,6 +30,11 @@ class SearchAttention(nn.Module):
         self.num_key_value_groups = self.num_heads // self.num_key_value_heads
         self.pretraining_tp = config.pretraining_tp
         self.max_position_embeddings = config.max_position_embeddings
+        self.att_type = att_type
+        self.rope_type = None
+        self.rope_config = None
+
+        assert self.att_type in ["full", "search", "sparse"], f"Unknown attention type {att_type}, should be in [full, search, sparse]"
 
         if (self.head_dim * self.num_heads) != self.hidden_size:
             raise ValueError(
@@ -52,18 +57,24 @@ class SearchAttention(nn.Module):
 
     def _init_rope(self):
         # rope_theta is default to 1e4, as set in RoPE kernel API.
-        if self.config.rope_scaling is None:
-            self.rotary_emb = LlamaRotaryEmbedding(
-                self.head_dim, max_position_embeddings=self.max_position_embeddings
-            )
+        rope_scaling = getattr(self.config, "rope_scaling", None)
+        if rope_scaling is None:
             self.rope_scale = 1.0
+            self.rope_type = "llama2"
         else:
-            scaling_type = self.config.rope_scaling["type"]
-            if scaling_type == "linear":
-                # support for Longchat-v1.5.
-                self.rope_scale = self.config.rope_scaling["factor"]
-            else:
-                raise ValueError(f"Unknown RoPE scaling type {scaling_type}")
+            if rope_scaling is not None:
+                if "type" in rope_scaling:
+                    rope_type = rope_scaling["type"]
+                elif "rope_type" in rope_scaling:
+                    rope_type = rope_scaling["rope_type"]
+                else:
+                    raise ValueError(
+                        "rope_scaling must have a 'type' or 'rope_type' key.")
+                assert rope_type in ["llama3"]
+                self.rope_type = rope_type
+                self.rope_config = rope_scaling
+                self.rope_scale = rope_scaling["factor"]
+                self.rope_theta = getattr(self.config, "rope_theta", None)
 
     def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
         return (
@@ -84,10 +95,10 @@ class SearchAttention(nn.Module):
     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
         bsz, q_len, _ = hidden_states.size()
 
-        assert bsz == 1, "SearchAttention only supports batch size 1."
+        assert bsz == 1, "TDAttention only supports batch size 1."
         assert hasattr(
             self, "layer_idx"
-        ), "SearchAttention requires layer_idx to inference."
+        ), "TDAttention requires layer_idx to inference."
 
         if self.pretraining_tp > 1:
             key_value_slicing = (
@@ -125,21 +136,38 @@ class SearchAttention(nn.Module):
             torch.cuda.nvtx.range_pop()
 
         # Not transposed for Append kv cache NHD layout
+        # if q_len == 1 and self.att_type == "sparse":
+        #     print(self.layer_idx, hidden_states[0, 0, :5])
         query_states = query_states.view(q_len, self.num_heads, self.head_dim)
         key_states = key_states.view(q_len, self.num_key_value_heads, self.head_dim)
         value_states = value_states.view(q_len, self.num_key_value_heads, self.head_dim)
 
         torch.cuda.nvtx.range_push("RoPE")
-        tidal.utils.apply_rope_in_place(
-            query_states,
-            key_states,
-            iController.kv_cache.seqlen - q_len,
-            rope_scale=self.rope_scale,
-        )
+        if self.rope_type == "llama2":
+            tidal.utils.apply_rope_in_place(
+                query_states,
+                key_states,
+                iController.kv_cache.seqlen - q_len,
+                rope_scale=self.rope_scale,
+            )
+        elif self.rope_type == "llama3":
+            tidal.utils.apply_llama31_rope_in_place(
+                query_states,
+                key_states,
+                iController.kv_cache.seqlen - q_len,
+                rope_scale=self.rope_scale,
+                rope_theta=self.rope_theta,
+                low_freq_factor=self.rope_config["low_freq_factor"],
+                high_freq_factor=self.rope_config["high_freq_factor"],
+                old_context_length=self.rope_config["original_max_position_embeddings"]
+            )
         torch.cuda.nvtx.range_pop()
 
+        # query_states = torch.ones_like(query_states).to(query_states.device)
+        # key_states = torch.ones_like(key_states).to(key_states.device)  
+
         torch.cuda.nvtx.range_push("append_kv")
-        # tidal manages KV-Cache internal (with PageAttention)
+        # Tidal manages KV-Cache internal (with PageAttention)
         # Here we do not concat / stack
         # We concat after RoPE
         tidal.utils.append_kv(
@@ -149,6 +177,8 @@ class SearchAttention(nn.Module):
             self.layer_idx,
         )
         torch.cuda.nvtx.range_pop()
+
+        ## You are here
 
         # Prefill/Decode kernels is different
         if q_len > 1:
@@ -162,25 +192,62 @@ class SearchAttention(nn.Module):
             torch.cuda.synchronize()
             torch.cuda.nvtx.range_pop()
         else:
-            torch.cuda.nvtx.range_push("search_attn")
-            torch.cuda.synchronize()
-            attn_output = tidal.utils.decode_sparse_attn(
-                query_states,
-                iController,
-                self.layer_idx,
-                iController.kv_indices_without_last,
-                True,
-            )
-            torch.cuda.synchronize()
-            torch.cuda.nvtx.range_pop()
+            # Decode Stage
+            # Skipping layers is controled by PAGE_BUDGET, which is set in LlamaModel.
+            if self.att_type == "full":
+                torch.cuda.nvtx.range_push("full_attn")
+                torch.cuda.synchronize()
+                attn_output = tidal.utils.decode_sparse_attn(
+                    query_states,
+                    iController,
+                    self.layer_idx,
+                    iController.kv_indices_without_last,
+                    False,
+                )
+                torch.cuda.synchronize()
+                torch.cuda.nvtx.range_pop()
+            elif self.att_type == "search":
+                torch.cuda.nvtx.range_push("search_attn")
+                torch.cuda.synchronize()
 
-            torch.cuda.nvtx.range_push("arg_topk")
-            torch.cuda.synchronize()
-            tidal.utils.decode_topk(
-                iController,
-            )
-            torch.cuda.synchronize()
-            torch.cuda.nvtx.range_pop()
+                attn_output = tidal.utils.decode_sparse_attn(
+                    query_states,
+                    iController,
+                    self.layer_idx,
+                    iController.kv_indices_without_last,
+                    True,
+                )
+                torch.cuda.synchronize()
+                torch.cuda.nvtx.range_pop()
+
+                torch.cuda.nvtx.range_push("arg_topk")
+                torch.cuda.synchronize()
+                # tidal.utils.decode_topk(
+                #     iController,
+                # )
+                _, top_k_indices = torch.topk(iController.qk_product, k=31, dim=-1)
+                iController.top_k_indices = top_k_indices.int()
+                # if self.layer_idx == 2:
+                #     print(self.layer_idx, iController.qk_product[0, :61]/1.44336, query_states[0, 0, :20], key_states[0, 0, :20])
+                # exit(0)
+                torch.cuda.synchronize()
+                torch.cuda.nvtx.range_pop()
+                # iController.top_k_indices = torch.ones((32, 32)).to(attn_output.device).int()
+                
+            elif self.att_type == "sparse":
+                # print(self.layer_idx, iController.top_k_indices)
+                assert iController.top_k_indices is not None, "top_k_indices should be set in search stage."
+                torch.cuda.nvtx.range_push("sparse_attn")
+                torch.cuda.synchronize()
+                attn_output = tidal.utils.decode_sparse_attn(
+                    query_states,
+                    iController,
+                    self.layer_idx,
+                    iController.top_k_indices,
+                    False,
+                )
+                torch.cuda.synchronize()
+                torch.cuda.nvtx.range_pop()
 
         attn_output = attn_output.unsqueeze(0)  # unsqueeze the batch dimension
         # FlashInfer output is naturally NHD
